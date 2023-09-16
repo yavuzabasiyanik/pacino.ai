f71acd18fed941edc5d8a258f3d2fdb1950c8b80
sifuhotman <abasiyanikyavuz@gmail.com>
2023-09-16 02:24:00 -0400

    added two buttons and pages

---
+++ pages/continue-recruiting.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContinueRecruiting = () => {
+  return (
+    <div>
+      {/* Your content for the "Continue Recruiting" page goes here */}
+    </div>
+  );
+};
+
+export default ContinueRecruiting;
